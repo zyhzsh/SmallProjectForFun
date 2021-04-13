e702a8c61f0b628e4619657d7c2d25f7e8aa4a1b
Zhu,Shenghang S <s.zhu@student.fontys.nl>
2021-04-13 11:51:56 +0200

    UnFinshed Gui Setup Alrothm Still need to adjust

---
+++ GuessNumberGameBinarySearch/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace GuessNumberGameBinarySearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStartGuess = new System.Windows.Forms.Button();
+            this.lbresult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(428, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pick up your pen and write a positive integer in a paper.(Range Between 1~4000000" +
+    "000)";
+            // 
+            // btnStartGuess
+            // 
+            this.btnStartGuess.Location = new System.Drawing.Point(49, 51);
+            this.btnStartGuess.Name = "btnStartGuess";
+            this.btnStartGuess.Size = new System.Drawing.Size(219, 29);
+            this.btnStartGuess.TabIndex = 1;
+            this.btnStartGuess.Text = "Start~!";
+            this.btnStartGuess.UseVisualStyleBackColor = true;
+            this.btnStartGuess.Click += new System.EventHandler(this.btnStartGuess_Click);
+            // 
+            // lbresult
+            // 
+            this.lbresult.AutoSize = true;
+            this.lbresult.Location = new System.Drawing.Point(335, 59);
+            this.lbresult.Name = "lbresult";
+            this.lbresult.Size = new System.Drawing.Size(0, 13);
+            this.lbresult.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 104);
+            this.Controls.Add(this.lbresult);
+            this.Controls.Add(this.btnStartGuess);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStartGuess;
+        private System.Windows.Forms.Label lbresult;
+    }
+}
+
---
+++ GuessNumberGameBinarySearch/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessNumberGameBinarySearch
+{
+    public partial class Form1 : Form
+    {
+        uint minNumber;
+        uint maxNumber;
+        uint guessnumber;
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBoxManager.Yes = "Big";
+            MessageBoxManager.No = "Small";
+            MessageBoxManager.Register();
+        }
+
+        private void btnStartGuess_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            minNumber = 0;
+            maxNumber = 4000000000;
+            guessnumber = (maxNumber + minNumber) / 2;
+            while (AskUserGuessResult()) {
+                counter++;
+                if (counter == 31) { break; }
+            }
+            MessageBox.Show("The number on you paper is: "+ guessnumber);
+        }
+
+        private bool AskUserGuessResult() {
+
+            DialogResult dr = MessageBox.Show("Is Your Result: " + guessnumber + "? Or it's too",
+          "Guessing...", MessageBoxButtons.YesNo);
+            switch (dr)
+            {
+                case DialogResult.Yes:
+                    maxNumber = guessnumber;
+                    guessnumber = (minNumber + guessnumber) / 2;                
+                    break;
+                case DialogResult.No:
+                    minNumber = guessnumber;
+                    guessnumber = (maxNumber + maxNumber) / 2;
+                    break;
+            }
+            return true;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
